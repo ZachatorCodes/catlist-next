a597ae8fed7058f59a04198a2ca3c0cbb98bbc07
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-31 13:36:15 -0600

    Created basic navbar component

---
+++ src/comps/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>Cat List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Cat Listings</a>
+    </nav>
+   );
+}
+ 
+export default Navbar;
