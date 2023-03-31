0ae5ffbfd5b8cacd57cb3016e17318d405aa3931
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-31 14:18:46 -0600

    added links to different pages

--- src/comps/Navbar.tsx
+++ src/comps/Navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
       <div>
         <h1>Cat List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Cat Listings</a>
+      <Link href="">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/cats">Cat Listings</Link>
     </nav>
    );
 }
