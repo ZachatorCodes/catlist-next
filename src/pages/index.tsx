2c559259700e2917acbfd276c69fa01e1764a118
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-31 13:41:07 -0600

    imported navbar and footer components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import Footer from "@/comps/Footer";
+import Navbar from "@/comps/Navbar"
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home Page</h1>
       <p>Lorem ipsum</p>
+      <Footer />
     </div>
   );
 }
