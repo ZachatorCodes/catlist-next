9c2fb483e587767ec871dd6ed0a30b119d5ae462
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-31 14:53:14 -0600

    removed duplicate header and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>Lorem ipsum</p>
       <Link href="/cats">See Cat Listings</Link>
-      <Footer />
     </div>
   );
 }
