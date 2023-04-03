5a04fcad5767a2f43449dd60993cdaf0b43b4c81
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-03 16:11:09 -0600

    changed h1 logo to Image logo

--- src/comps/Navbar.tsx
+++ src/comps/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Cat List</h1>
+        <Image src="/bunkface.png" alt="logo" width={60} height={55}/>
       </div>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
