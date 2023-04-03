b0ff62f7a7ae474a63b4000e67958f887e702a3f
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-03 15:22:20 -0600

    imported styles and added classnames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/comps/Footer";
 import Navbar from "@/comps/Navbar";
 import Link from "next/link";
+import styles from "@/styles/Home.module.css"
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum</p>
-      <Link href="/cats">See Cat Listings</Link>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>Lorem ipsum</p>
+      <Link href="/cats" className={styles.btn}>See Cat Listings</Link>
     </div>
   );
 }
