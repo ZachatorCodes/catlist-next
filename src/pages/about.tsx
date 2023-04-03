263ff46196af7e6dd525f4fdc08000dedf966414
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-03 16:13:04 -0600

    updates title based on page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,19 @@
+import Head from "next/head";
+
 const About = () => {
-  return ( 
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-      <p>Lorem ipsum</p>
-    </div>
+  return (
+    <> 
+      <Head>
+      <title>Cat List | About</title>
+        <meta name="keywords" content="cats" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+      </div>
+    </>
    );
 }
  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import Footer from "@/comps/Footer";
-import Navbar from "@/comps/Navbar";
+import Head from "next/head";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.text}>Lorem ipsum</p>
-      <Link href="/cats" className={styles.btn}>See Cat Listings</Link>
-    </div>
+    <>
+      <Head>
+        <title>Cat List | Home</title>
+        <meta name="keywords" content="cats" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>Lorem ipsum</p>
+        <Link href="/cats" className={styles.btn}>See Cat Listings</Link>
+      </div>
+    </>
   );
 }
