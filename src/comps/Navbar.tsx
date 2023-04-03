e2f7c58cc7d379fba77abce9fd293ed602d17adb
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-03 12:34:49 -0600

    added class name to logo

--- src/comps/Navbar.tsx
+++ src/comps/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav>
-      <div>
+      <div className="logo">
         <h1>Cat List</h1>
       </div>
       <Link href="/">Home</Link>
