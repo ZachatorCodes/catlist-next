3e94109824e2eff6cbedb7ee49265a180fe3dacb
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-04 10:46:52 -0600

    fetched data with getStaticProps

--- src/pages/cats/index.tsx
+++ src/pages/cats/index.tsx
@@ -1,9 +1,24 @@
-import styles from "@/styles/Cats.module.css"
+import styles from "@/styles/Cats.module.css";
 
-const Cats = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { cats: data },
+  };
+};
+
+const Cats = ({ cats }) => {
   return (
     <div>
       <h1>All Cats</h1>
+      {cats.map((cat) => (
+        <div key={cat.id}>
+          <a className={styles.single}>
+            <h3>{cat.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
